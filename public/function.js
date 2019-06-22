46d08cdfdd808af3b1ed5626cb8c5104d7accdab
hbracy <hbracy1@gmail.com>
2019-06-22 17:53:45 -0400

    heroku

--- public/function.js
+++ public/function.js
@@ -12,7 +12,7 @@ const serverHostname = 'thequicktalk.herokuapp.com'
 
 const serverPort = 3000;
 
-const socket = io("https://" + serverHostname + ":" + serverPort);
+const socket = io("https://" + serverHostname);
 
 console.log(socket);
 
